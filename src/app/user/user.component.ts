25472f4e61921fd82a0060910fb006551df384a3
endijsbertans <endijsbertans@gmail.com>
2024-07-21 14:07:29 +0300

    NoSignal: added output

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, Input, input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 
 @Component({
@@ -8,15 +8,14 @@ import {Component, computed, Input, input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar();
-  // });
+  @Output() select = new EventEmitter();
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
-  onSelectUser(){}
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
 }

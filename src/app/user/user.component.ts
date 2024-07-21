4668ca0faac98740b1bbadf720a5af1a90ad4a2a
endijsbertans <endijsbertans@gmail.com>
2024-07-21 13:54:35 +0300

    removed signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from "../dummy-users";
-const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, computed, Input, input} from '@angular/core';
+
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,17 +8,15 @@ const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
-
-  // zone.js style Angular < 16
-  // get imagePath() {
-  //   return 'assets/users/'+ this.selectedUser.avatar
-  // }
-
-
-  onSelectUser(){
-    const  randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
   }
+  onSelectUser(){}
 }
